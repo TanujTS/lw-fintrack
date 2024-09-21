3cf97abf96a644f82416e6f3d8ea871b5e8b8214
Ishu <tattooless.kid@gmail.com>
2024-09-21 12:12:06 +0530

    Refactor scaffold background color in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSwatch().copyWith(
           secondary: Colors.teal
         ),
-        scaffoldBackgroundColor: Colors.black,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 63, 63, 63),
       ),
       home: MainScreen(),
     );
